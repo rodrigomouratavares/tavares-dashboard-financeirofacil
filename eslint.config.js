fef285930761593ae5e5e6e7a2759ee88a051ed0
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-05-28 23:48:53 -0300

    correcao

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+// eslint.config.js
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { FlatCompat } from '@eslint/eslintrc' // <-- Importa adaptador
+import prettier from 'eslint-config-prettier'
+
+// Cria compatibilidade para usar extends antigos no Flat Config
+const compat = new FlatCompat()
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  ...compat.extends('prettier'), // <-- Adiciona config do Prettier
+)
