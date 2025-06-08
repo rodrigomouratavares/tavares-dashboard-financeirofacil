b30d7be2b9f3902966a4a63e4aa855c76b51a8b6
Rodrigo Moura Tavares <rodrigomoura.t@gmail.com>
2025-06-08 15:54:54 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-export { default as Home } from './home'
+export { default as Home } from './Home'
 export { default as Leads } from './Leads'
 export { default as Login } from './Login'
 export { default as Registration } from './Registration'
