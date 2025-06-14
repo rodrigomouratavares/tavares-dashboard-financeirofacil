f65f707fb71a10f2705f2aac5dd9c37c7ff818d2
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-06-14 19:17:33 -0300

    Dividindo o login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
 
 const LoginArea = styled.div`
   background: #666;
+  color: white;
 `
 
 const LoginImage = styled.div`
@@ -13,10 +17,38 @@ const LoginImage = styled.div`
 
 function Login() {
   return (
-    <>
-      <LoginArea>LOGIN</LoginArea>
-      <LoginImage />
-    </>
+    <Box sx={{ height: '100vh' }}>
+      <Grid container sx={{ height: '100%' }}>
+        {/* Lado esquerdo - área de login */}
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Container maxWidth="sm">
+            <LoginArea>LOGIN</LoginArea>
+          </Container>
+        </Grid>
+
+        {/* Lado direito - imagem */}
+        <Grid
+          item
+          xs={false}
+          sm={6}
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            height: '100vh',
+          }}
+        >
+          <LoginImage />
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
