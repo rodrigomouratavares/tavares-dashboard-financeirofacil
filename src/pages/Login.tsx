7f9889f34200c15c1d25ef0e7949a0bb5af7ff89
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-06-23 23:12:24 -0300

    Login e image com mui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
+<<<<<<< HEAD
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 
+=======
+import { Box, Container, Grid } from '@mui/material'
+>>>>>>> 0b18129 (Login e image com mui)
 const LoginArea = styled.div`
   background: #666;
 `
@@ -29,6 +33,7 @@ function Login() {
               <LoginArea>LOGIN</LoginArea>
             </Container>
           </Grid>
+<<<<<<< HEAD
 
           {/* Lado direito - imagem */}
           <Grid
@@ -41,6 +46,10 @@ function Login() {
             <Container maxWidth="sm">
               <LoginImage />
             </Container>
+=======
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <LoginImage />
+>>>>>>> 0b18129 (Login e image com mui)
           </Grid>
         </Grid>
       </Box>
