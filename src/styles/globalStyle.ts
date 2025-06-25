e91c268bec7acec5fcbf716ece19a1dabab16793
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-06-24 22:44:23 -0300

    Global banner

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-import { DefaultTheme } from 'styled-components'
+import type { DefaultTheme } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
         body, html {
