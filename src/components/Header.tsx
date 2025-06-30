17af611e061baf3671d2bc0dabdb8c42b2a88f27
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-06-30 19:10:13 -0300

    Header e logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { Logo } from '@/components'
+import { Link } from 'react-router-dom'
+import { Avatar, Box, Container } from '@mui/material'
+import { pxToRem } from '@/utils'
+
+export const StyledHeader = styled.header`
+  background-color: ${(props) => props.theme.appBackground};
+  border-bottom: ${pxToRem(1)} solid ${(props) => props.theme.appDefaultStroke};
+  margin-bottom: ${pxToRem(37)}
+  width:100%
+  
+`
+function Header() {
+  return (
+    <StyledHeader>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'space-between',
+            height: pxToRem(64),
+          }}
+        >
+          <Link to="/home">
+            <Logo height={25} width={100} />
+          </Link>
+          <Link to="/perfil">
+            <Avatar
+              alt="TAVARES AVATAR"
+              src="/Avatar.svg"
+              sx={{ width: pxToRem(40), height: pxToRem(40) }}
+            ></Avatar>
+          </Link>
+        </Box>
+      </Container>
+    </StyledHeader>
+  )
+}
+
+export default Header
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as FormComponent } from './FormComponent' //procurar entender por que dessa forma
+export { default as Header } from './Header'
 export * from './BannerImage'
+export * from './Logo'
 export * from './StyledButton'
 export * from './StyledInput'
 export * from './Typographies'
