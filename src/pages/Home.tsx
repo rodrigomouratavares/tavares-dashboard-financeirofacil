5db1f3d7bac8808f7574de166ed38baeaaded634
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-07-02 10:50:36 -0300

    feat/table-component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { AvatarList, CardComponent, Header } from '@/components'
+import { AvatarList, CardComponent, CustomTable, Header } from '@/components'
 import { Container } from '@mui/material'
 import { currencyConverter } from '@/utils'
 function Home() {
@@ -19,6 +19,27 @@ function Home() {
       subtitle: currencyConverter(3452.54),
     },
   ]
+
+  const mockTableData = {
+    headers: ['name', 'Email', 'Actions'],
+    rows: [
+      [
+        <span>Nome 1</span>,
+        <span>nome1@gmail.com</span>,
+        <button>ACTION</button>,
+      ],
+      [
+        <span>Nome 2</span>,
+        <span>nome2@gmail.com</span>,
+        <button>ACTION</button>,
+      ],
+      [
+        <span>Nome 3</span>,
+        <span>nome3@gmail.com</span>,
+        <button>ACTION</button>,
+      ],
+    ],
+  }
   return (
     <>
       <Header />
@@ -26,6 +47,13 @@ function Home() {
       <Container maxWidth="lg">
         <CardComponent>
           <AvatarList listData={mockListData} />
+        </CardComponent>
+
+        <CardComponent>
+          <CustomTable
+            headers={mockTableData.headers}
+            rows={mockTableData.rows}
+          />
         </CardComponent>
       </Container>
     </>
