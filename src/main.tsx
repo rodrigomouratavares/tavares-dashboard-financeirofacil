832b6eab77e7d58ac7b6c2c7ca9d59d8ab17ae48
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-07-03 22:13:01 -0300

    correcao

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/globalStyle.ts'
