2da7b3ca9a4fe66fd30df04ae74528b3999a0622
Rodrigo Tavares <rodrigomoura.t@gmail.com>
2025-09-25 22:40:37 -0300

    Feat create profile-flow

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/globalStyle.ts'
 import { AppThemeProvider } from './context/AppThemeContext.tsx'
-
+import { Provider } from 'react-redux'
+import store from './redux/index.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppThemeProvider>
-      <GlobalStyle />
-      <App />
-    </AppThemeProvider>
+    <Provider store={store}>
+      <AppThemeProvider>
+        <GlobalStyle />
+        <App />
+      </AppThemeProvider>
+    </Provider>
   </StrictMode>
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ChangeEvent, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import Cookies from 'js-cookie'
-// components
-import { Box, Container, Grid } from '@mui/material'
+
+// MUI
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+// COMPONENTS
 import {
   BannerImage,
   FormComponent,
+  Logo,
   StyledH1,
   StyledP,
-  Logo,
 } from '@/components'
 
 // HOOKS
@@ -18,28 +24,37 @@ import { useFormValidation, usePost } from '@/hooks'
 // UTILS
 import { jwtExpirationDateConverter, pxToRem } from '@/utils'
 
-// Types
-import { DecodedJWT, MessageProps, LoginData, LoginPostData } from '@/types'
+// REDUX
+import { useSelector } from 'react-redux'
+import { RootState } from '@/redux'
+
+// TYPES
+import { DecodedJWT, LoginData, LoginPostData, MessageProps } from '@/types'
 
 function Login() {
-  const navigate = useNavigate()
-
   const inputs = [
     { type: 'email', placeholder: 'Email' },
     { type: 'password', placeholder: 'Senha' },
   ]
-
   const { data, loading, error, postData } = usePost<LoginData, LoginPostData>(
     'login'
   )
   const { formValues, formValid, handleChange } = useFormValidation(inputs)
+  const { email, message } = useSelector(
+    (state: RootState) => state.createProfile
+  )
+  const navigate = useNavigate()
 
   const handleMessage = (): MessageProps => {
-    if (!error) return { msg: '', type: 'success' }
+    if (!error)
+      return {
+        msg: message ?? '',
+        type: 'success',
+      }
     switch (error) {
       case 401:
         return {
-          msg: 'Email e/ou senha inválida.',
+          msg: 'Email e/ou senha inválidos.',
           type: 'error',
         }
       default:
@@ -69,58 +84,57 @@ function Login() {
     if (Cookies.get('Authorization')) navigate('/home')
   }, [data, navigate])
 
+  useEffect(() => {
+    if (email) {
+      handleChange(0, email)
+    }
+  }, [email])
+
   return (
-    <Box>
-      <Grid container>
-        {/* Lado esquerdo - login */}
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
-        >
-          <Container maxWidth="sm">
-            <Box sx={{ marginBottom: pxToRem(24) }}>
-              <Logo height={25} width={100} />
-            </Box>
-            <Box sx={{ marginBottom: pxToRem(24) }}>
-              <StyledH1>Bem vindo</StyledH1>
-              <StyledP>Digite seu email e senha para logar</StyledP>
-            </Box>
-
-            <FormComponent
-              inputs={inputs.map((input, index) => ({
-                type: input.type,
-                placeholder: input.placeholder, // <-- corrigido
-                value: formValues[index] || '',
-                onChange: (e: ChangeEvent<HTMLInputElement>) =>
-                  handleChange(index, (e.target as HTMLInputElement).value),
-              }))}
-              buttons={[
-                {
-                  className: 'primary',
-                  disabled: !formValid || loading,
-                  type: 'submit',
-                  onClick: handleSubmit,
-                  children: loading ? 'Aguarde...' : 'Login',
-                },
-              ]}
-              message={handleMessage()}
-            />
-          </Container>
+    <>
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <Logo height={41} width={100} />
+              </Box>
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <StyledH1>Bem-vindo</StyledH1>
+                <StyledP>Digite sua senha e email para logar</StyledP>
+              </Box>
+              <FormComponent
+                inputs={inputs.map((input, index) => ({
+                  type: input.type,
+                  placeholder: input.placeholder,
+                  value: formValues[index] || '',
+                  onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                    handleChange(index, (e.target as HTMLInputElement).value),
+                }))}
+                buttons={[
+                  {
+                    className: 'primary',
+                    disabled: !formValid || loading,
+                    type: 'submit',
+                    onClick: handleSubmit,
+                    children: loading ? 'Aguarde...' : 'Login',
+                  },
+                ]}
+                message={handleMessage()}
+              />
+            </Container>
+          </Grid>
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <BannerImage />
+          </Grid>
         </Grid>
-
-        {/* Lado direito - imagem */}
-        <Grid
-          item
-          xs={false}
-          sm={6}
-          sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-          <BannerImage />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   )
 }
 
